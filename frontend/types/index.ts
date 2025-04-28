6c9af8b751e2c4a53e7794f440df845739381822
Vinicius Bregoli <vinibregoli@gmail.com>
2025-04-28 20:38:27 +0200

    sim

--- frontend/types/index.ts
+++
@@ -1,8 +0,0 @@
-
-export interface DetectionState {
-    selectedImage: File | null;
-    preview: string | null;
-    resultImage: string | null;
-    isLoading: boolean;
-    error: string | null;
-  }
